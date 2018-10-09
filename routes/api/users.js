7a2db127221e5e30fb39c7cac35a4dc48c81ebf6
Quan Trinh <trinhquan1123@yahoo.com>
2018-10-09 17:48:38 +0200

    starting on login, finished registration route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,43 @@
 const express = require('express');
-const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
-router.get('/test', (req, res) => res.json({msg: "Users works!"}));
+const router = express.Router();
+const User = require('../../models/User');
+
+// Registration
+router.post('/register', (req, res) => {
+	User.findOne({ email: req.body.email })
+		.then(user => {
+			if (user) {
+				return res.status(400).json({ email: "Email alerady exists." })
+			}
+			else {
+				const avatar = gravatar.url(req.body.email, {
+					s: '200',
+					r: 'r',
+					d: 'mm'
+				});
+
+				const newUser = new User({
+					name: req.body.name,
+					email: req.body.email,
+					password: req.body.password,
+					avatar: avatar
+				});
+
+				bcrypt.genSalt(10, (err, salt) => {
+					bcrypt.hash(newUser.password, salt, (err, hash) => {
+						if(err) throw err;
+						newUser.password = hash;
+						newUser.save()
+							.then(user => res.json(user))
+							.catch(err => console.log(err));
+					})
+				})
+			}
+		})
+});
+
 
 module.exports = router;
