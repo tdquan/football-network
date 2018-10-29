9b52f6c2bb555c488ee6c840151b476ce4fd0324
Quan Trinh <trinhquan1123@yahoo.com>
2018-10-29 02:50:49 +0100

    basic routes done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,7 @@ const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const validatePostInput = require('../../validation/post');
 
-router.get('/test', (req, res) => res.json({msg: "Posts works!"}));
+router.get('/test', (req, res) => res.json({ msg: "Posts works!" }));
 
 // Create a post
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -67,7 +67,7 @@ router.post('/upvote/:id', passport.authenticate('jwt', { session: false }), (re
 				.then(post => {
 					if (post.votes.filter(vote => vote.user.toString() === req.user.id).length > 0) {
 						const removeIndex = post.votes
-							.map(item => item.user.toString())
+							.map(vote => vote.user.toString())
 							.indexOf(req.user.id);
 						post.votes.splice(removeIndex, 1);
 						post.save().then(post => res.json(post));
@@ -88,7 +88,7 @@ router.post('/downvote/:id', passport.authenticate('jwt', { session: false }), (
 				.then(post => {
 					if (post.votes.filter(vote => vote.user.toString() === req.user.id).length > 0) {
 						const removeIndex = post.votes
-							.map(item => item.user.toString())
+							.map(vote => vote.user.toString())
 							.indexOf(req.user.id);
 						post.votes.splice(removeIndex, 1);
 						post.save().then(post => res.json(post));
@@ -99,6 +99,50 @@ router.post('/downvote/:id', passport.authenticate('jwt', { session: false }), (
 				})
 				.catch(err => res.status(404).json({ nopost: "No post found with that id." }));
 		});
+});
+
+// Add a comment to a post
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	// Validation
+	const { errors, isValid } = validatePostInput(req.body);
+	if (!isValid) {
+		return res.status(400).json(errors);
+	}
+
+	Post.findById(req.params.id)
+		.then(post => {
+			const newComment = {
+				text: req.body.text,
+				name: req.body.name,
+				avatar: req.body.avatar,
+				user: req.user.id
+			}
+
+			// Add comment to array
+			post.comments.unshift(newComment);
+			post.save().then(post => res.json(post));
+		})
+		.catch(err => res.status(404).json({ nopost: 'No post found with that id.' }));
+});
+
+// Delete a comment of a post
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	Post.findById(req.params.id)
+		.then(post => {
+			if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+				return res.status(404).json({ commentnotfound: 'Cannot find comment.' });
+			}
+
+			// Get removeIndex
+			const removeIndex = post.comments
+				.map(comment => comment._id.toString())
+				.indexOf(req.params.comment_id);
+
+			// Remove from the comments array
+			post.comments.splice(removeIndex, 1);
+			post.save().then(post => res.json(post));
+		})
+		.catch(err => res.status(404).json({ nopost: 'No post found with that id.' }));
 });
 
 module.exports = router;
