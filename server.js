0d72a4df4f11619911e0942502a4790c9873f355
Quan Trinh <trinhquan1123@yahoo.com>
2018-10-30 18:19:25 +0100

    added react client

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const db = require('./config/keys').mongoURI;
 
 mongoose
