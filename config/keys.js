618819e63a6db51e08a2219c9b83a1e5a21b1145
Quan Trinh <trinhquan1123@yahoo.com>
2018-11-15 10:42:34 +0100

    working on profile interface

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-	mongoURI: 'mongodb://tdquan:mlabTh30merta@ds125713.mlab.com:25713/football-news',
+	// mongoURI: 'mongodb://tdquan:mlabTh30merta@ds125713.mlab.com:25713/football-news',
+	mongoURI: 'mongodb://127.0.0.1:27017/football-news',
 	secret: 'secret'
 }
