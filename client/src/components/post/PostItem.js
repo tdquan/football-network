6996c1bcfa571b3c8a0ea53c5c0492f80aba809e
Quan Trinh <trinhquan1123@yahoo.com>
2018-11-23 19:02:36 +0100

    todo: delete post functionality

--- client/src/components/post/PostItem.js
+++ client/src/components/post/PostItem.js
@@ -1,13 +1,65 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 class PostItem extends Component {
 	constructor(props) {
 		super(props);
 	}
 
+	onDeleteClick(id) {
+		console.log(id);
+	}
+
 	render() {
-		return <div>Hello there</div>;
+		const { post, auth } = this.props;
+
+		return (
+			<div className="card card-body mb-3">
+				<div className="row">
+					<div className="col-md-2">
+						<Link to={`/users/${post.user}`}>
+							<img src={post.avatar} alt="avatar" className="rounded-circle d-none d-md-block" />
+						</Link>
+						<br />
+						<p className="text-center">{post.name}</p>
+					</div>
+					<div className="col-md-10">
+						<p className="lead">{post.text}</p>
+						<button className="btn btn-light mr-1">
+							<i className="text-secondary fas fa-thumbs-up" />
+							<span className="badge badge-light">{post.votes.length}</span>
+						</button>
+						<button className="btn btn-light mr-1">
+							<i className="text-secondary fas fa-thumbs-down" />
+						</button>
+						<Link to={`/post/${post._id}`} className="btn btn-info mr-1">
+							Comments
+						</Link>
+						{post.user === auth.user.id ? (
+							<button
+								onClick={this.onDeleteClick.bind(this, post._id)}
+								className="btn btn-danger mr-1"
+							>
+								<i className="fas fa-times" />
+							</button>
+						) : null}
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
-export default PostItem;
+PostItem.propTypes = {
+	post: PropTypes.object.isRequired,
+	auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth
+});
+
+export default connect(mapStateToProps)(PostItem);
