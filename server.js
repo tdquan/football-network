3076a846ef83e63c84e5e73435e472a5fa099d00
Quan Trinh <trinhquan1123@yahoo.com>
2018-12-15 18:17:39 +0700

    fixed heroku build config

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -17,7 +18,10 @@ const PORT = process.env.PORT || 8080;
 const db = require('./config/keys').mongoURI;
 
 mongoose
-	.connect(db, { useNewUrlParser: true })
+	.connect(
+		db,
+		{ useNewUrlParser: true }
+	)
 	.then(() => console.log('\n\x1b[32m%s\n\x1b[0m', '---- *** Connected to MongoDB. *** ----'))
 	.catch(err => console.log('\n\x1b[31m%s\n\x1b[0m', err));
 
@@ -31,4 +35,15 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-app.listen(PORT, () => console.log('\n\x1b[32m%s\n\x1b[0m', `---- *** Server running on port ${PORT}... *** ----`));
+// Server static assets in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+app.listen(PORT, () =>
+	console.log('\n\x1b[32m%s\n\x1b[0m', `---- *** Server running on port ${PORT}... *** ----`)
+);
